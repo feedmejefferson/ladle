082e80f122a7ef1b73922b59820e584b7d833882
Jeff Hansen <dscheffy@gmail.com>
2019-03-05 11:11:55 +0000

    Initial commit of ladle
    It works for happy paths, but needs a little more robustness in terms of
    error handling and help messages regarding usage. It can import/export a
    collection of documents to and from a folder of json files though.

---
+++ index.ts
@@ -0,0 +1,53 @@
+import admin from 'firebase-admin';
+import fs from 'fs';
+import minimist from 'minimist';
+import { Observable, bindNodeCallback, of } from 'rxjs';
+import { map, tap, switchMap, flatMap } from 'rxjs/operators';
+
+
+var args = minimist(process.argv.slice(2), {
+      string: 'cert',
+      default: { cert: './serviceAccountKey.json' }
+//    string: 'lang',           // --lang xml
+//    boolean: ['version', 'stuff'],     // --version
+//    alias: { v: 'version', s: 'stuff' } 
+  });
+  
+admin.initializeApp({
+    credential: admin.credential.cert(args.cert)
+})
+
+const command = args._[0]; // import or export
+// depending on import or export, the source and target are either a firestore
+// collection or a local folder
+const source = args._[1]; 
+const target = args._[2]; 
+const db = admin.firestore();
+
+const filesInDir$ = bindNodeCallback(fs.readdir);
+const readFile$ = bindNodeCallback(fs.readFile);
+const writeFile$ = bindNodeCallback(fs.writeFile);
+
+if (command==='export') {
+    // TODO: it would be nice if we created the directory first if it didn't exist
+    const results = db.collection(source).get()
+    .then(collection => collection.forEach(item => {
+        writeFile$(target + '/' + item.id, JSON.stringify(item.data()))
+        .subscribe()
+    }))
+    .catch(err => console.log(err));
+} else {
+    filesInDir$(target).pipe(
+        flatMap(x => x),   // flatten the list of files to individual files
+    ).subscribe(id => of(id).pipe(  // create a new closure so we can hang on to the file name to use as an id
+        map(f => target + '/' + f),   // get the full relative path to the file
+        flatMap(f => readFile$(f)),  // unnest the new observable created by readFile$
+        map((b => JSON.parse(b.toString()))),  // convert the buffer to a string and parse it
+        ).subscribe(data => {
+            const results = db.collection(target).doc(id).set(data)
+            .then(x => console.log('success!', x))
+            .catch(error => console.log(error));
+        }));
+
+}
+
